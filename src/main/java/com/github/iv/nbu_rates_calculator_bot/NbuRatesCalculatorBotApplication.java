fdf6ba57e9b1eeed086884e183d611d2a64a3448
Iryna Ustymenko <i.ustimenko18@gmail.com>
2022-10-11 21:00:46 +0300

    create NBUclass

---
+++ src/main/java/com/github/iv/nbu_rates_calculator_bot/NbuRatesCalculatorBotApplication.java
@@ -0,0 +1,14 @@
+package com.github.iv.nbu_rates_calculator_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbuRatesCalculatorBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbuRatesCalculatorBotApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/github/iv/nbu_rates_calculator_bot/NbuRatesCalculatorBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.iv.nbu_rates_calculator_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbuRatesCalculatorBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
